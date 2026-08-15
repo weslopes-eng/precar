9b7fd9cbdeacba8018bc04939965072765e92454
weslopes-eng <weslopes-eng@users.noreply.github.com>
2026-08-15 13:03:07 -0300

    Liga o site à chave pública do Supabase.

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 window.PRECAR_SUPABASE = {
   url: "https://khuiwgoyhonvhckslmye.supabase.co",
-  anonKey: ""
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtodWl3Z295aG9udmhja3NsbXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MDE4NjYsImV4cCI6MjEwMjM3Nzg2Nn0.VcUA9o6PICE-htm04M3X56wUh3y8nnxX3u3Ik1ZeE34"
 };
